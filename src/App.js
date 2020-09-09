88edbc66a80ebf445d882d8359015b4503bc6d66
charith <charithcherry1100gmail.com>
2020-09-09 18:47:49 +0530

    dishes1.5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent'
-function App() {
-  return (
+import {DISHES} from './shared/dishes'
+class App extends Component  {
+
+  constructor(props){
+    super(props)
+    this.state={
+      dishes:DISHES
+    };
+  }
+  render(){
+    return(
     <div>
       <Navbar dark color='primary'>
         <div className='container'>
@@ -12,9 +21,9 @@ function App() {
           </div>
 
       </Navbar>
-    <Menu/>
+    <Menu dishes={this.state.dishes}/>
     </div>
   );
-}
+}}
 
 export default App;
