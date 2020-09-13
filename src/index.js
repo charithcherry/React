4b1d0014a66048df96dce2c7a3037656b381eb37
charith <charithcherry1100gmail.com>
2020-09-13 15:59:44 +0530

    Controlled forms and validation

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
