c443879c6c6aacefc0acc3837a5c675c08ef5f51
charith <charithcherry1100gmail.com>
2020-09-15 01:00:07 +0530

    redux thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle ,Breadcrumb,BreadcrumbItem} from 'reactstrap';
-import {Link} from 'react-router-dom';    
+import {Link} from 'react-router-dom'; 
+import {Loading} from './LoadingComponent';   
 
     function RenderMenuItem ({dish, onClick}) {
         return (
@@ -19,20 +20,40 @@ import {Link} from 'react-router-dom';
 
     const Menu = (props) => {
 
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1"  key={dish.id}>
                     <RenderMenuItem dish={dish}/>
                 </div>
             );
         });
-
+        if (props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row"> 
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else{
         return (
             <div className="container">
                 <div className="row">
                     <Breadcrumb>
                     <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem acive>Menu</BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
                     </Breadcrumb>
                     <div className="col-12">
                         <h3>Menu</h3>
@@ -43,6 +64,6 @@ import {Link} from 'react-router-dom';
                 </div>
             </div>
         );
-    }
+    }}
 
 export default Menu;
