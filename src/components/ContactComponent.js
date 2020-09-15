5fb60a90eeb0fc79440c52bc63a0609b4a3850b3
charith <charithcherry1100gmail.com>
2020-09-16 01:09:32 +0530

    assignment4

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -9,18 +9,23 @@ import {
 } from "reactstrap";
 import { Control, Form, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
-
-const required = val => val && val.length;
-const maxLength = len => val => !val || val.length <= len;
-const minLength = len => val => val && val.length >= len;
-const isNumber = val => !isNaN(Number(val));
-const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
-
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
+
+  constructor(props){
+      super(props);
+      
+
+      
+      this.handleSubmit=this.handleSubmit.bind(this);
   }
+
+
+
 
   handleSubmit(values) {
     this.props.resetFeedbackForm();
@@ -35,22 +40,50 @@ class Contact extends Component {
     );
   }
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <Breadcrumb>
-            <BreadcrumbItem>
-              <Link to="/home">Home</Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem active>Contact Us</BreadcrumbItem>
-          </Breadcrumb>
-          <div className="col-12">
-            <h3>Contact Us</h3>
-            <hr />
-          </div>
-        </div>
-        <div className="row row-content">
+
+
+
+    render(){
+
+
+    return(
+        <div className="container">
+            <div className="row">
+                    <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+<BreadcrumbItem active>Contactus</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+<h3>Contactus</h3>
+                    </div>
+                </div>
+            <div className="row row-content">
+                <div className="col-12">
+                <h3>Location Information</h3>
+                </div>
+                <div className="col-12 col-sm-4 offset-sm-1">
+                        <h5>Our Address</h5>
+                        <address>
+                        121, Clear Water Bay Road<br />
+                        Clear Water Bay, Kowloon<br />
+                        HONG KONG<br />
+                        <i className="fa fa-phone"></i>: +852 1234 5678<br />
+                        <i className="fa fa-fax"></i>: +852 8765 4321<br />
+                        <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
+                        </address>
+                </div>
+                <div className="col-12 col-sm-6 offset-sm-1">
+                    <h5>Map of our Location</h5>
+                </div>
+                <div className="col-12 col-sm-11 offset-sm-1">
+                    <div className="btn-group" role="group">
+                        <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
+                        <a role="button" className="btn btn-info" href="www.google.com"><i className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
+                    </div>
+                </div>
+            </div>
+            <div className="row row-content">
           <div className="col-12">
             <h3>Send us your Feedback</h3>
           </div>
